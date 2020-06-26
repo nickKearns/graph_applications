3728dbb0801f8a1ac9b06c653ebe63c1db82b777
nickKearns <nickkearns133@gmail.com>
2020-06-26 11:54:54 -0400

    solved first problem

--- graph_questions.py
+++ graph_questions.py
@@ -28,6 +28,7 @@
 #hopefully I can do it in a way so that I can iterate over each vertex and then finding the connected
 #components will be much simpler after that is done
 
+from collections import deque
 
 class Vertex(object):
     def __init__(self, vertex_id):
@@ -38,13 +39,18 @@ class Vertex(object):
         self.neighbors_dict = {}
 
     def get_neighbors(self):
-        return self.neighbors_dict.keys()
+        return self.neighbors_dict.values()
 
     def add_neighbor(self, vertex):
         self.neighbors_dict[vertex.id] = vertex
 
+    def add_edge(self, vertex):
+        self.add_neighbor(vertex)
+        vertex.add_neighbor(self)
+
     def __str__(self):
-        return str(self.id)
+        neighbor_ids = list(self.neighbors_dict.keys())
+        return f'{self.id} adjacent to {neighbor_ids}'
     def __repr__(self):
         return self.__str__()
 
@@ -55,47 +61,137 @@ def turn_matrix_into_graph(matrix: [[int]]):
     graph = {}
     for col in range(len(matrix[0])):
         for row in range(len(matrix)):
-            print(row, col)
+            # print(row, col)
             # print(matrix[row][col])
-            if matrix[row][col] == 1 and (row, col) not in graph:
+            if matrix[row][col] == 1:
                
-                new_vertex = Vertex((row, col))
-                graph[(row, col)] = new_vertex
 
-                #check all the vertices in the 4 directions around the current one for 1's
-                #so that we can add them as neighbors
+                if (row, col) not in graph:
+                    current_vertex = Vertex((row, col))
 
-               
-                #check the neighbor beneath the vertex
-                if 0 >= row+1 < len(matrix):
-                    if matrix[row+1][col] == 1:
-                        below_neighbor = Vertex((row+1, col))
-                        graph[(row+1, col)] = below_neighbor
-                        new_vertex.add_neighbor(below_neighbor)
+                    #check all the vertices in the 4 directions around the current one for 1's
+                    #so that we can add them as neighbors
 
-                # check the neighbor above the vertex
-                if 0 >= row-1 < len(matrix):
-                    if matrix[row-1][col] == 1:
-                        above_neighbor = Vertex((row-1, col))
-                        graph[(row-1, col)] = above_neighbor
-                        new_vertex.add_neighbor(above_neighbor)
 
-                #check the neighbor to the left of the vertex
-                if 0 >= col-1:
-                    if matrix[row][col-1] == 1:
-                        left_neighbor = Vertex((row, col-1))
-                        graph[(row, col-1)] = left_neighbor
-                        new_vertex.add_neighbor(left_neighbor)
+                    #check the neighbor beneath the vertex
+                    if row+1 < len(matrix):
+                        # print("checking beneath")
+                        if matrix[row+1][col] == 1:
+                            if (row+1, col) in graph:
+                                current_vertex.add_edge(graph[(row+1, col)])
+                            else:
+                                below_neighbor = Vertex((row+1, col))
+                                graph[(row+1, col)] = below_neighbor
+                                # current_vertex.add_neighbor(below_neighbor)
+                                current_vertex.add_edge(below_neighbor)
+                    # check the neighbor above the vertex
+                    # print(row-1)
+                    if 0 <= row-1:
+                        # print("checking above")
+                        if matrix[row-1][col] == 1:
 
-                #check the neighbor to the right of the vertex
-                if col+1 < len(matrix):
-                    if matrix[row][col+1]:
-                        right_neighbor = Vertex((row, col+1))
-                        graph[(row, col+1)] = right_neighbor
-                        new_vertex.add_neighbor(right_neighbor)
+                            if (row-1, col) in graph:
+                                current_vertex.add_edge(graph[(row-1, col)])
+
+                            else:
+                                above_neighbor = Vertex((row-1, col))
+                                graph[(row-1, col)] = above_neighbor
+                                # current_vertex.add_neighbor(above_neighbor)
+                                current_vertex.add_edge(above_neighbor)
+
+                    #check the neighbor to the left of the vertex
+                    if 0 <= col-1:
+                        # print("checking to the left")
+                        if matrix[row][col-1] == 1:
+                        
+                            if (row, col-1) in graph:
+                                current_vertex.add_edge(graph[(row,col-1)])
+
+                            else:
+                                left_neighbor = Vertex((row, col-1))
+                                graph[(row, col-1)] = left_neighbor
+                                # current_vertex.add_neighbor(left_neighbor)
+                                current_vertex.add_edge(left_neighbor)
+
+                    #check the neighbor to the right of the vertex
+                    if col+1 < len(matrix[0]):
+                        # print("checking to the right")
+                        if matrix[row][col+1] == 1:
+
+                            if (row, col+1) in graph:
+                                current_vertex.add_edge(graph[(row, col+1)])
+
+                            else:
+
+                                right_neighbor = Vertex((row, col+1))
+                                graph[(row, col+1)] = right_neighbor
+                                # current_vertex.add_neighbor(right_neighbor)
+                                current_vertex.add_edge(right_neighbor)
+                    if current_vertex not in graph:
+                        graph[(row, col)] = current_vertex
 
     return graph
 
+#This function is used and combined with the above function to find the number of
+#islands in a 2d array of integers (matrix)
+def get_connected_components(graph):
+
+    #this set will contain vertex objects
+    visited = set()
+
+    connected_comps = []
+
+    def get_cc_recursive(current_vertex: Vertex, visited_vertices, connected_vertices):
+        visited_vertices.add(current_vertex)
+        connected_vertices.append(current_vertex.id)
+        print(current_vertex)
+
+        for neighbor in current_vertex.get_neighbors():
+            # print(neighbor.__str__() + " is a neighbor to " + current_vertex.__str__())
+            if neighbor not in visited_vertices:
+                get_cc_recursive(neighbor, visited_vertices, connected_vertices)
+        return connected_vertices
+
+    for vertex in graph.values():
+        if vertex not in visited:
+
+            current_connected_vertices = []
+            get_cc_recursive(vertex, visited, current_connected_vertices)
+
+            connected_comps.append(current_connected_vertices)
+
+
+
+
+# def bfs_traversal(graph):
+
+#     seen = set()
+#     start_vertex = graph[(0,0)]
+#     seen.add(start_vertex)
+
+#     distance_away = 0
+
+#     queue = deque()
+
+#     queue.append(start_vertex)
+
+
+#     while queue:
+#         current_vertex = queue.popleft()
+
+
+
+#         for neighbor in current_vertex.get_neighbors():
+#             if neighbor not in seen:
+#                 seen.add(neighbor)
+#                 queue.append(neighbor)
+#     return distance_away
+
+
+
+
+
+    return connected_comps
 
 
 
@@ -104,14 +200,41 @@ def turn_matrix_into_graph(matrix: [[int]]):
 
 if __name__ == "__main__":
 
-    map1 = [
-    [1, 1, 1, 1, 0],
-    [1, 1, 0, 1, 0],
-    [1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0]
-]
 
-    print(turn_matrix_into_graph(map1))
+
+    #THIS IS QUESTION 1:
+    #HOW MANY ISLANDS ARE THERE
+
+    # map1 = [
+    # [1, 1, 1, 1, 0],
+    # [1, 1, 0, 1, 0],
+    # [1, 1, 0, 0, 0],
+    # [0, 0, 0, 0, 0]
+    # ]
+    # graph = turn_matrix_into_graph(map1)
+    # print(len(get_connected_components(graph)))
+
+    # map2 = [
+    # [1, 1, 0, 0, 0],
+    # [1, 1, 0, 0, 0],
+    # [0, 0, 1, 0, 0],
+    # [0, 0, 0, 1, 1]
+    # ]
+    # graph2 = turn_matrix_into_graph(map2)
+    # print(len(get_connected_components(graph2)))
+
+
+    oranges_graph = [
+        [1,1,1],
+        [1,1,0],
+        [0,1,1]
+    ]
+
+    graph = turn_matrix_into_graph(oranges_graph)
+    print(bfs_traversal(graph))
+
+
+    
 
 
                         
